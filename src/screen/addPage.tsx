bcf89a5526fc8ecd61dcbe1d68e693c1f13bfea4
송민재 <songminjae@songminjaeui-MacBookPro.local>
2020-07-23 21:43:10 +0900

    each day need

--- src/screen/addPage.tsx
+++ src/screen/addPage.tsx
@@ -3,15 +3,34 @@ import { useState } from "react";
 import { Button, View, TextInput, Text } from 'react-native';
 import { styles } from "./styles";
 
+import firestore from "@react-native-firebase/firestore";
+
 
 
 export function ProfileScreen({ navigation }) {
-  const [date, setDate] = useState("2020-01-01");
-  const [text, setText] = useState('default text');
-  const [time, setTime] = useState('0');
+  const [date, setDate] = useState<String>("2020-01-01");
+  const [text, setText] = useState<String>('default text');
+  const [time, setTime] = useState<String>('0');
 
-  console.log(date);
+  async function postDate(data : String){
+
+    fetch('http://35.213.58.175/api/todo/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
   
+  }
+
     return (
       
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -22,11 +41,12 @@ export function ProfileScreen({ navigation }) {
 
         <View style={styles.wrap}>
           <Text>Date</Text>
-          <TextInput style={styles.input} onChangeText ={input => setDate(input)}>date?</TextInput>
+          <TextInput style={styles.input} onChangeText ={input => setDate(input)}></TextInput>
           <Text>Text</Text>
-          <TextInput style={styles.input} onChangeText ={input => setText(input)}>text?</TextInput>
+          <TextInput style={styles.input} onChangeText ={input => setText(input)}></TextInput>
           <Text>time</Text>
-          <TextInput style={styles.input}  onChangeText={input=>setTime(input)} >time?</TextInput>
+          <TextInput style={styles.input}  onChangeText={input=>setTime(input)} ></TextInput>
+          <Button title="add schedule" onPress={() => postDate({"date": date, "text": text, "time": time})} />
 
           <Button title="return" onPress={() => navigation.goBack()} />
         </View>
--- src/screen/mainPage.tsx
+++ src/screen/mainPage.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import { Button, View, TextInput, Text, Alert, TouchableOpacity} from 'react-native';
+import { Button, View, TextInput, Text, Alert, TouchableOpacity, ListView} from 'react-native';
 import { styles } from "./styles";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 import CalendarStrip from 'react-native-slideable-calendar-strip';
 import { FlatList } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import firestore from "@react-native-firebase/firestore";
 
 export var selectedDate = new Date();
 
@@ -15,14 +15,49 @@ export interface dateCheck{
 }
 
 export function HomeScreen({ navigation }) {
-  const [today, setToday] = useState(0);
-  const [transferdate, setTransferdate] = useState(0);
-  const [data, setData] = useState(
-    [
-      {key:"0","data": "2020-09-07", "text": "핀토스", "time": "01"},
-      {key:"1","data": "2020-09-07", "text": "핀토스", "time": "01"},
-    ],
-  );
+  const [transferdate, setTransferdate] = useState("2020-07-23");
+  // const [ todo, setTodo ] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [todos, setTodos] = useState([]);
+  const db = firestore().collection('todos');
+
+  // const [data, setData] = useState(
+  //   [
+  //     {data: "2020-09-07", text: "핀토스", time: "01"},
+  //     {data: "2020-09-07", text: "핀토스", time: "02"},
+  //   ],
+  // );
+
+  console.log("transferdate touch:",transferdate);
+    
+    async function addTodo(){
+      await db.add({Date: "2020-07-23", Text: "밥", Time: "01"});
+      console.log('suc add');
+    }
+    
+    useEffect(() => {
+      return db.onSnapshot((querySnapshot) => {
+        const list = [];
+        // querySnapshot.query.where("Date", "==", transferdate).orderBy("Date");
+        querySnapshot.forEach(doc =>{ 
+          const {Date, Text, Time} =doc.data();
+
+          list.push({
+            id: doc.id,
+            Date,
+            Text,
+            Time,
+          });
+        });
+
+        setTodos(list);
+
+        if(loading){
+          setLoading(false);
+        }
+        console.log(todos);
+      });
+    }, []);
 
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -34,17 +69,18 @@ export function HomeScreen({ navigation }) {
           selectedDate={selectedDate}
           onPressDate={(date: any) => {
             selectedDate = date;
-            setToday(date);
+            // setToday(date);
+            setTransferdate(date);
           }}
           onPressGoToday={(today: any) => {
             selectedDate = today;
             setTransferdate(today);
           }}
           onSwipeDown={() => {
-            Alert('onSwipeDown');
+            Alert.alert('onSwipeDown');
           }}
           markedDate={['2018-05-04', '2018-05-15', '2018-06-04', '2018-05-01']}
-          weekStartsOn={1} // 0,1,2,3,4,5,6 for S M T W T F S, defaults to 0
+          weekStartsOn={0} // 0,1,2,3,4,5,6 for S M T W T F S, defaults to 0
         />        
         
         <Button
@@ -53,23 +89,26 @@ export function HomeScreen({ navigation }) {
         />
 
        <SafeAreaView style={styles.wrap}> 
-        
           
-          <FlatList
-            data={data}
-            keyExtractor={(item)=> item.name}
-            renderItem={({item})=>{
-              return (
-                <TouchableOpacity style={styles.sloth} onPress={()=>{alert(item.data)}}>
-                  <View style={{borderWidth:1, borderRadius: 8, padding:8, margin:8}}>
-                    <Text>{item.key}</Text>
-                    <Text>{item.data}</Text>
-                    <Text>{item.text}</Text>
-                    <Text>{item.time}</Text>
-                  </View>
-                </TouchableOpacity>
-              );
-            }}></FlatList>
+          <Button title="add todo" onPress={()=>addTodo()}></Button>
+
+            <FlatList
+              data = {todos}
+              keyExtractor = {(item)=>item.id}
+              renderItem={({item}) =>{
+                return(
+                  <TouchableOpacity onPress={()=>{Alert.alert('delete?')}}>
+                    <View style={{borderWidth:1, borderRadius: 8, padding:8, margin:8}}>
+                      {/* <Text>{item.key}</Text> */}
+                      <Text>Date: {item.Date}</Text>
+                      <Text>Text: {item.Text}</Text>
+                      <Text>Time: {item.Time}</Text>
+                    </View>                    
+                  </TouchableOpacity>
+                );
+              }} 
+
+            ></FlatList>
 
         </SafeAreaView>
       
